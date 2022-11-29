e5a1f6741b5d13bd1f2d1452cfc0bb24222bfcf5
PaulineVarin <PaulineVarin@172.17.142.129>
2022-11-29 15:19:56 +0100

    Initialisation des classes

---
+++ src/elementsJeu/JeuOie.java
@@ -0,0 +1,26 @@
+package elementsJeu;
+
+public class JeuOie {
+	private Oie[] oies; 
+	private int nbOie = 0; 
+	private Plateau plateau = new Plateau();
+	private De des = new De();
+	
+	public JeuOie(int nbOieMax) {   
+		oies = new Oie[nbOieMax];
+	 }
+	
+	public void ajouterOie(String couleur) { 
+		if (nbOie < oies.length) {
+			oies[nbOie] = new Oie(couleur, plateau, des);
+			nbOie++;
+		}
+	}
+
+	public void jouer() {
+	}
+	 
+}
+
+
+
---
+++ src/main/mainJeu.java
@@ -0,0 +1,15 @@
+package main;
+
+import elementsJeu.*;
+
+public class mainJeu {
+
+	public static void main(String[] args) {
+		JeuOie jeuOie = new JeuOie(3);
+		jeuOie.ajouterOie("verte");
+		jeuOie.ajouterOie("jaune");
+		jeuOie.ajouterOie("bleu");
+		jeuOie.jouer();
+	}
+
+}
